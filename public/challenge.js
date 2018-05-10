f1b369287fcf47b953462a759ed1a2f47eff5b74
Angelo Vartuca <angelo.v@hotmail.co.uk>
2018-05-10 15:34:27 +0100

    jQuery submit details
    Use jQuery to submit those details to the /newsletter endpoint on click of the button

--- public/challenge.js
+++ public/challenge.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
+
+
+
+	$(".newsletter").submit(function(e){
+		e.preventDefault();
+		var name = $(".namefield").val();
+		var email = $(".emailfield").val();
+
+		 $.ajax({
+		    type: "POST",
+		    url: "http://localhost:8080/newsletter",
+		    data: JSON.stringify({"name": name, "email": email}),
+		    contentType: 'application/json'
+	   	});
+	});
+
+
   $.ajax({
     type: "POST",
     url: "http://localhost:8080/analytics",
     data: JSON.stringify({url:"http://localhost:8080/challenge.html"}),
     contentType: 'application/json'
    });
+
+
 });
+
+// onlclick event on signup . 1-add listener to the click
+
+/* $( "#other" ).click(addEventListener() {
+  $( "#signup" ).click();
+}); */
+
+
+
+
+//what data to submit
+
+// submit those details to the /newsletter endpoint button
